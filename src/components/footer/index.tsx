54b6d95fa4d76abf22ce25c69cc6c65f036cd793
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-01 17:29:07 -0300

    feat: :sparkles: create Footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,8 @@
+import styles from "./footer.module.css";
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright 2021 - Elikem Daniels</p>
+    </footer>
+  );
+}
