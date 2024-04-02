38fff5a1a81d511b8f02a5084200e78f73a26261
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-02 08:29:15 -0300

    fix: :lipstick: fix cursor in Contact button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { PostList } from "@/components/postList";
+import { PostList, Contact } from "@/components";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <PostList />
+      <Contact />
     </main>
   );
 }
